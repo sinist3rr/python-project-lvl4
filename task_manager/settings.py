0cc74a5c0e9cf6c87d446a5dd5284e690a120050
sinist3rr <darkfloki@protonmail.com>
2021-08-10 15:06:17 +0300

    django secret

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+x2t0r$3hak9@!vnobyp#^ebo01y&n)%5a(cm+mx+yr=!^$hpo'  # noqa E501
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
